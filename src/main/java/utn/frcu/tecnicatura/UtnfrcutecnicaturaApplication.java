991770ba30671178a3d27f1c8f1e124566ab0af1
Franco Armando <francoarmando1911@gmail.com>
2025-03-17 21:16:18 -0300

    Inicializando el proyecto

---
+++ src/main/java/utn/frcu/tecnicatura/UtnfrcutecnicaturaApplication.java
@@ -0,0 +1,13 @@
+package utn.frcu.tecnicatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtnfrcutecnicaturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtnfrcutecnicaturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utn/frcu/tecnicatura/UtnfrcutecnicaturaApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.frcu.tecnicatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtnfrcutecnicaturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
