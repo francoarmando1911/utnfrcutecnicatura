c4634ae169084b51697d664ef52663cbafa46678
Franco Armando <francoarmando1911@gmail.com>
2025-03-28 07:40:29 -0300

    Inicializacion de ejercicio1 clase Persona

---
+++ src/main/java/Ejercicio1/MainPersona.java
@@ -0,0 +1,17 @@
+package Ejercicio1;
+/* Clase Main:
+   - Crear 3 objetos de la clase Persona:
+   - El primero con todos los atributos seteados.
+   - El segundo con nombre, edad y sexo (sin peso ni altura).
+   - El tercero usando el constructor por defecto y luego setear los valores usando los métodos set.
+
+Para cada objeto:
+ - Comprobar si está en su peso ideal, tiene sobrepeso o está por debajo del peso ideal. Mostrar un mensaje.
+ - Indicar si es mayor de edad.
+ - Mostrar la información completa del objeto (usando toString()).*/
+
+public class MainPersona {
+    public static void main(String[] args) {
+
+    }
+}
