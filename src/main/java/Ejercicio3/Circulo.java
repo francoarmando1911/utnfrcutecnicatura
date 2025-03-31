23fe7fc4a9c63144beb43988d1e1dc5e7eb4c167
Franco Armando <francoarmando1911@gmail.com>
2025-03-31 20:49:39 -0300

    Practico 3 - clases, triangulo, rectangulo y circulo, cada una con sus constructores y getters y setters, a su vez, cada una con sus metodos para calcular perimetro y calcular area. Clase Main de creacion e instanciacion de objetos con sus respectivos metodos creados anteriormente en las clases ya mencionadas. Por ultimo imprimir los datos

---
+++ src/main/java/Ejercicio3/Circulo.java
@@ -0,0 +1,38 @@
+package Ejercicio3;
+
+public class Circulo {
+    double radio;
+    final double PI = 3.14159265359;
+
+
+    public Circulo() {
+    }
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    //C = 2*pi*r
+    public static double calcularPerimetro(double PI, double radio) {
+        double perimetro;
+        perimetro = 2 * PI * radio;
+        return perimetro;
+    }
+
+    //A = PI*(r*r)
+    public static double calcularArea(double PI, double radio) {
+        double area;
+        area = PI * (radio * radio);
+        return area;
+    }
+
+
+}
---
+++ src/main/java/Ejercicio3/MainGeometria.java
@@ -0,0 +1,39 @@
+package Ejercicio3;
+
+/*
+Crear clases  `Circulo`, `Rectangulo` y `Triangulo`  cada una de estas debe tener los siguientes metodos:
+   - `double calcularArea()`
+   - `double calcularPerimetro()`
+
+   - `Circulo` debe tener un atributo `radio`
+   - `Rectangulo` debe tener atributos `base` y `altura`
+   - `Triangulo` debe tener atributos `lado1`, `lado2`, `lado3`
+
+ En la clase `Main`, crear instancias de cada forma y mostrar por pantalla sus áreas y perímetros.
+ */
+
+import static java.lang.Math.PI;
+
+public class MainGeometria {
+    public static void main(String[] args) {
+        Circulo circulo = new Circulo();
+        Triangulo triangulo = new Triangulo();
+        Rectangulo rectangulo = new Rectangulo();
+
+        double perimetroCirculo = Circulo.calcularPerimetro(PI, 15);
+        double areaCirculo = Circulo.calcularArea(PI, 15);
+        System.out.println("El perimetro del circulo es: " + perimetroCirculo);
+        System.out.println("El area del circulo es: " + areaCirculo);
+
+        double perimetroRectangulo = Rectangulo.calcularPerimetro(27, 40);
+        double areaRectangulo = Rectangulo.calcularArea(27, 40);
+        System.out.println("El periemtro del rectangulo es: " + perimetroRectangulo);
+        System.out.println("El area del rectangulo es: " + areaRectangulo);
+
+        double perimetroTriangulo = Triangulo.calcularPerimetro(15, 25, 30);
+        double areaTriangulo = Triangulo.calcularArea(15, 25, 30);
+        System.out.println("El perimetro del triangulo es: " + perimetroTriangulo);
+        System.out.println("El area del triangulo es: " + areaTriangulo);
+
+    }
+}
