9719f88c477c5ab2711ad49f9df3096dd3959cd3
Franco Armando <francoarmando1911@gmail.com>
2025-04-14 21:52:52 -0300

    Actualizacion

--- src/main/java/Practico6_Vehiculos/Motor.java
+++ src/main/java/Practico6_Vehiculos/Motor.java
@@ -1,4 +1,10 @@
 package Practico6_Vehiculos;
 
+/*
+calcularRevolucionesMotor(int fuerza, int radio): el
+número de re- voluciones se calcula como la
+multiplicación de la fuerza del motor por su radio.
+ */
+
 public interface Motor {
 }
--- src/main/java/Practico6_Vehiculos/Vela.java
+++ src/main/java/Practico6_Vehiculos/Vela.java
@@ -1,4 +1,9 @@
 package Practico6_Vehiculos;
 
+/*
+recomendarVelocidad(int velocidadViento): si la
+velocidad del vien- to es mayor a 80 km/h es muy alta,
+se recomienda no salir a navegar
+ */
 public interface Vela {
 }
