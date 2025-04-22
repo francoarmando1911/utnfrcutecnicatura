cfbb10137822e6b92c47f6fb4abce261daab9e18
Franco Armando <francoarmando1911@gmail.com>
2025-04-21 21:17:43 -0300

    Actualizacion ejercicio correlativas

---
+++ src/main/java/Practico7ValidadorCorrelativas/Materia.java
@@ -0,0 +1,32 @@
+package Practico7ValidadorCorrelativas;
+
+import java.util.List;
+
+public class Materia {
+
+    private String nombre;
+    private List<Materia> correlativas;
+
+    public Materia(String nombre, List<Materia> correlativas) {
+        this.nombre = nombre;
+        this.correlativas = correlativas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Materia> getCorrelativas() {
+        return correlativas;
+    }
+
+    public void setCorrelativas(List<Materia> correlativas) {
+        this.correlativas = correlativas;
+    }
+
+    //metodo puedeCursar(Alumno: boolean)
+}
