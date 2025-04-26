3be334852bcbf18a4d7ece1915c16f7721c77189
Franco Armando <francoarmando1911@gmail.com>
2025-04-26 17:49:49 -0300

    Codificacion main

--- src/main/java/EjerciciosComposicion/Ejercicio1_EdificioInteligente/MainEjercicioInteligente.java
+++ src/main/java/EjerciciosComposicion/Ejercicio1_EdificioInteligente/MainEjercicioInteligente.java
@@ -2,6 +2,14 @@ package EjerciciosComposicion.Ejercicio1_EdificioInteligente;
 
 public class MainEjercicioInteligente {
     public static void main(String[] args) {
+        EdificioInteligente edificio = new EdificioInteligente();
 
+        Departamento depto1 = new Departamento();
+        Departamento depto2 = new Departamento();
+
+        edificio.agregarDepartamento(depto1);
+        edificio.agregarDepartamento(depto2);
+
+        edificio.listarDepartamentos();
     }
 }
