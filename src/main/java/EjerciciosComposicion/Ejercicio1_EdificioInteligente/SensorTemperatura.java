c5acaf0e019fcf3ff1c243aa29ef91d461822b7e
Franco Armando <francoarmando1911@gmail.com>
2025-04-26 17:39:39 -0300

    Actualizacion

--- src/main/java/EjerciciosComposicion/Ejercicio1_EdificioInteligente/SensorTemperatura.java
+++ src/main/java/EjerciciosComposicion/Ejercicio1_EdificioInteligente/SensorTemperatura.java
@@ -1,4 +1,8 @@
 package EjerciciosComposicion.Ejercicio1_EdificioInteligente;
 
-public class SensorTemperatura {
+public class SensorTemperatura extends SensorAmbiental{
+    public SensorTemperatura(String tipoSensor, float valor) {
+        super(tipoSensor, valor);
+    }
+
 }
