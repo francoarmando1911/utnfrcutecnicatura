757d5e2621643e916c91c92e4d6aa8c17b164684
Franco Armando <francoarmando1911@gmail.com>
2025-04-28 20:12:17 -0300

    Actualizacion

--- src/main/java/EjerciciosComposicion/Ejercicio2_Videojuegos/Enemigo.java
+++ src/main/java/EjerciciosComposicion/Ejercicio2_Videojuegos/Enemigo.java
@@ -1,4 +1,14 @@
 package EjerciciosComposicion.Ejercicio2_Videojuegos;
 
 public class Enemigo  {
+
+    private String nombre;
+    private int poder;
+
+    public Enemigo(String nombre, int poder) {
+        this.nombre = nombre;
+        this.poder = poder;
+    }
+
+    //atacar():void
 }
--- src/main/java/EjerciciosComposicion/Ejercicio2_Videojuegos/Nivel.java
+++ src/main/java/EjerciciosComposicion/Ejercicio2_Videojuegos/Nivel.java
@@ -1,4 +1,16 @@
 package EjerciciosComposicion.Ejercicio2_Videojuegos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Nivel {
+    private List<Enemigo> enemigos;
+
+    public Nivel(List<Enemigo> enemigos) {
+        this.enemigos = new ArrayList<>();
+    }
+
+    //agregarEnemigo(enemigo: Enemigo): void
+    //eliminarEnemigo(enemigo: Enemigo):: void
+    //obtenerEnemigo(): List<Enemigo>
 }
