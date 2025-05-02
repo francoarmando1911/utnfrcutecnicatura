2a079e0b13279ae8f1f2f3880f7998778161b8eb
Franco Armando <francoarmando1911@gmail.com>
2025-05-02 07:59:22 -0300

    update

--- src/main/java/EjerciciosAgregacion/Ejercicio1_SIstemaEscolar/Escuela.java
+++ src/main/java/EjerciciosAgregacion/Ejercicio1_SIstemaEscolar/Escuela.java
@@ -1,4 +1,24 @@
 package EjerciciosAgregacion.Ejercicio1_SIstemaEscolar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Escuela {
+    private String nombre;
+    private String direccion;
+    private List<Curso> cursos;
+    private List<Profesor> profesores;
+
+    public Escuela(String nombre, String direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.cursos = new ArrayList<>();
+        this.profesores = new ArrayList<>();
+    }
+
+    // Getters y métodos para añadir cursos/profesores
+    public List<Curso> getCursos() { return cursos; }
+    public List<Profesor> getProfesores() { return profesores; }
+    public void addCurso(Curso curso) { cursos.add(curso); }
+    public void addProfesor(Profesor profesor) { profesores.add(profesor); }
 }
