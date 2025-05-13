1480ba491bf872120de5b60789c2fb72e1864b7d
Franco Armando <francoarmando1911@gmail.com>
2025-05-13 18:16:15 -0300

    update

--- src/main/java/PracticoENUMS/RobotCafe/MainRobotCafe.java
+++ src/main/java/PracticoENUMS/RobotCafe/MainRobotCafe.java
@@ -2,8 +2,7 @@ package PracticoENUMS.RobotCafe;
 
 public class MainRobotCafe {
     public static void main(String[] args) {
-        Pedido pedido = new Pedido(TipoVaso.PEQUENIO, TipoCafe.LATTE);
-        Stock stock = new Stock();
+
     }
 
 }
